fb059a972d05a3975fe84f835fd79dbdc12d4517
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2015-10-30 11:15:51 +0000

    Set the node server modules and configuration by use of a boilerplate

---
+++ server.js
@@ -0,0 +1,54 @@
+// modules =================================================
+var express        = require('express');
+var mongoose       = require('mongoose');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+// configuration ===========================================
+    
+// config files
+var db = require('./config/db');
+
+// set our port
+var port = process.env.PORT || 8080; 
+
+// connect to our mongoDB database 
+// (uncomment after you enter in your own credentials in config/db.js)
+mongoose.connect(db.url); 
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser());
+
+// required for passport
+app.use(session({ secret: 'verysecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
+
+// routes ==================================================
+require('./app/routes')(app, passport); // configure our routes
+
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);
+
+// shoutout to the user
+console.log('Magic happens on port ' + port);
+
+// expose app
+exports = module.exports = app;
